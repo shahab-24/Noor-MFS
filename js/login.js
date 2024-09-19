e5bbc7affc267bb245f46f73cac9e431cfc23e3b
Shahab Uddin <shahab.82946@gmail.com>
2024-09-19 07:14:03 +0600

    manually phone number and pin match then it will go to another html page

--- js/login.js
+++ js/login.js
@@ -1,4 +1,15 @@
 document.getElementById('btn_login').addEventListener('click', function(e){
   e.preventDefault()
- 
+  
+  
+  const phoneNumber = document.getElementById('phone_number').value;
+  const pinNumber = document.getElementById('pin_number').value;
+  if( phoneNumber === '5' && pinNumber === '1234'){
+    console.log('please Login');
+    window.location.href ='/home.html';
+  }
+  else{
+    alert('wrong credentials')
+  }
+
 })
